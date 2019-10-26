a878883434e7e4037b226a8a171da42b4e9e47fb
AllanCh <edochacon96@gmail.com>
2019-10-26 08:51:13 -0600

    Agregar gets del catalogo

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,61 @@ var session = driver.session();
 
 router.get('/', (req, res) => res.json({ message: 'compratecapi' }));
 
-router.post('/compratecapi/sucursales', async (req, res) => {
+router.get('/compratecapi/sucursales', (req, res) => {
+    try {
+        const resultPromise = session.run(
+            'MATCH(n:Sucursal) RETURN n'
+        );
+        resultPromise.then(function (result) {
+            session.close();
+            // on application exit:
+            driver.close();
+            res.json({ result })
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.json({ message: 'fail' })
+    }
+});
+
+router.get('/compratecapi/productos', (req, res) => {
+    try {
+        const resultPromise = session.run(
+            'MATCH(n:Producto) RETURN n'
+        );
+        resultPromise.then(function (result) {
+            session.close();
+            // on application exit:
+            driver.close();
+            res.json({ result })
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.json({ message: 'fail' })
+    }
+});
+
+router.get('/compratecapi/servicios', (req, res) => {
+    try {
+        const resultPromise = session.run(
+            'MATCH(n:Servicio) RETURN n'
+        );
+        resultPromise.then(function (result) {
+            session.close();
+            // on application exit:
+            driver.close();
+            res.json({ result })
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.json({ message: 'fail' })
+    }
+})
+
+router.post('/compratecapi/sucursales', (req, res) => {
     const { id, name } = req.body;
     try {
         const resultPromise = session.run(
